d26042422495ce50372ef2c4dc9ea1b3a23545d9
Helmi <omistaja@email>
2021-08-13 11:10:57 +0300

    Paljon muutoksia kerralla kääks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const[kadenssi, setKadenssi] = useState(isNaN);
+  const[vanne, setVanne] = useState(isNaN);
+  const[rengas, setRengas] = useState(isNaN);
+  const[eturatas, setEturatas] = useState(isNaN);
+  const[takaratas, setTakaratas] = useState(isNaN);
+  const[nopeus, setNopeus] = useState(isNaN)
+
+  function addThem(){
+    setNopeus((eturatas/takaratas*(vanne+(rengas*2))*Math.PI/1000*kadenssi/60*3.6).toFixed(2))};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nopeus {nopeus} km/h</h1>
+      <div className="number-inputs">
+        <h3>Kadenssi</h3>
+        <input 
+        type="number" 
+        placeholder="rpm" 
+        value = {kadenssi} 
+        onChange={e => setKadenssi(+e.target.value)}
+        />
+      </div>
+      <div>
+        <h3>Vanne</h3>
+        <input 
+        type="number" 
+        placeholder="mm" 
+        value = {vanne} 
+        onChange={e => setVanne(+e.target.value)}
+        />
+      </div>
+      <div>
+        <h3>Rengas</h3>
+        <input 
+        type="number" 
+        placeholder="mm" 
+        value = {rengas} 
+        onChange={e => setRengas(+e.target.value)}
+        />
+      </div>
+      <div>
+        <h3>Eturatas</h3>
+        <input 
+        type="number" 
+        placeholder="hammasluku" 
+        value = {eturatas} 
+        onChange={e => setEturatas(+e.target.value)}
+        />
+      </div>
+      <div>
+        <h3>Takaratas</h3>
+        <input 
+        type="number" 
+        placeholder="hammasluku" 
+        value = {takaratas} 
+        onChange={e => setTakaratas(+e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={addThem}>Laske</button>
+        
+      </div>
     </div>
   );
 }
